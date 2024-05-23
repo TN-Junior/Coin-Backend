f6ceff79cc8d103c3742266320d374363d71a829
Maria Clara de Araújo Monteiro <113060036+Kuria-chan18@users.noreply.github.com>
2024-05-23 15:05:32 +0000

    Feito uma parte do código de planoContas.

---
+++ src/main/java/com/example/coin/Model/Boleto.java
@@ -0,0 +1,5 @@
+package com.example.coin.Model;
+
+public class Boleto {
+    
+}
--- src/main/java/com/example/coin/Model/planoContas.java
+++ src/main/java/com/example/coin/Model/planoContas.java
@@ -1,12 +1,66 @@
 package com.example.coin.Model;
-
-import jakarta.persistence.*;
-
 import java.util.List;
 import java.util.ArrayList;
 
-@Entity
 public class planoContas {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+
+private int idConta;
+private String conta;
+private String tipo;
+private String descricao;
+private List<Boleto> boletos;
+private Empresa empresa;
+
+
+public planoContas(int idConta, String conta, String tipo, String descricao, List<Boleto> boletos, Empresa empresa) {
+
+this.idConta = idConta;
+this.conta = conta;
+this.tipo = tipo;
+this.descricao = descricao;
+this.boletos = boletos;
+this.empresa = empresa;
+
+}
+
+public int getidConta() {
+    return idConta;
+}
+
+public void setidConta(int idConta) {
+    this.idConta = idConta;
+}
+
+public String getConta() {
+    return conta;
+}
+
+public void setConta(String conta) {
+    this.conta = conta;
+}
+
+public String getTipo() {
+    return tipo;
+}
+
+public void setTipo(String tipo) {
+    this.tipo = tipo;
+}
+
+public String getDescricao() {
+    return descricao;
+}
+
+public void setDescricao(String descricao) {
+    this.descricao = descricao;
+}
+
+public List<Boleto> getBoletos() {
+    return boletos;
+}
+
+public void setBoletos(List<Boleto> boletos) {
+    this.boletos = boletos;
+}
+
+}
