6afe96d3b564924bec423aca8674f39d154f31ef
Edson Antônio da Silva Junior <111139554+Janoul@users.noreply.github.com>
2024-06-10 12:46:28 -0300

    Codigo do boleto versao 1
    Lógica completa do funcionamento do boleto.

---
+++ src/main/java/com/example/coin/repository/BoletoRepository.java
@@ -0,0 +1,11 @@
+package com.example.coin.repository;
+
+import com.example.coin.model.Boleto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoletoRepository extends JpaRepository<Boleto, Integer>
+{
+
+}
