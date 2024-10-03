92f632f39a313a6cf587f6f385ee8ca44dd8f38d
tn-junior <juju2004.tary@gmail.com>
2024-10-03 13:37:01 -0300

    Permiti acesso público ao endpoint / Delete e Edit funcionam

--- src/main/java/com/example/coin/controller/ContaController.java
+++ src/main/java/com/example/coin/controller/ContaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RestController
 @RequestMapping("/api/contas")
 public class ContaController {
@@ -24,4 +24,24 @@ public class ContaController {
     public Conta adicionarConta(@RequestBody Conta conta) {
         return contaRepository.save(conta);
     }
+
+    @PutMapping("/{id}")
+    public Conta atualizarConta(@PathVariable Long id, @RequestBody Conta conta) {
+        return contaRepository.findById(id)
+                .map(contaExistente -> {
+                    contaExistente.setConta(conta.getConta());
+                    contaExistente.setStatus(conta.getStatus());
+                    contaExistente.setCategoria(conta.getCategoria());
+                    contaExistente.setValor(conta.getValor());
+                    contaExistente.setVencimento(conta.getVencimento());
+                    return contaRepository.save(contaExistente);
+                })
+                .orElseThrow(() -> new RuntimeException("Conta não encontrada"));
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluirConta(@PathVariable Long id) {
+        contaRepository.deleteById(id);
+    }
 }
+
