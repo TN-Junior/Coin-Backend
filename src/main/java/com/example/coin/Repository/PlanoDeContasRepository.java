8cf470ead48e0822e19c1f2166662e27fa5a267a
Maria Clara de Araújo Monteiro <113060036+Kuria-chan18@users.noreply.github.com>
2024-10-11 20:01:23 +0000

    Retirada de arquivos desnecessários.

--- src/main/java/com/example/coin/Repository/PlanoDeContasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.coin.Repository;
-
-import com.example.coin.Model.PlanoDeContas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlanoDeContasRepository extends JpaRepository<PlanoDeContas, Integer> {
-}
