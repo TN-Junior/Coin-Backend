e1100c3ee2e6ed851eba5544f8b5e066850278c8
TN-Junior <juju2004.tary@gmail.com>
2024-10-18 10:41:24 -0300

    pagamentos

---
+++ src/main/java/com/example/coin/controller/PagamentosController.java
@@ -0,0 +1,32 @@
+package com.example.coin.controller;
+
+import com.example.coin.model.Pagamentos;
+import com.example.coin.service.PagamentosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pagamentos")
+@CrossOrigin(origins = "*") // Permite o CORS para o front-end
+public class PagamentosController {
+
+    @Autowired
+    private PagamentosService pagamentosService;
+    
+    @GetMapping
+    public List<Pagamentos> getAllPagamentos() {
+        return pagamentosService.getAllPagamentos();
+    }
+
+    @GetMapping("/{tipo}")
+    public List<Pagamentos> getPagamentosByTipo(@PathVariable String tipo) {
+        return pagamentosService.getPagamentosByTipo(tipo.toUpperCase());
+    }
+
+    @PostMapping
+    public Pagamentos addPagamento(@RequestBody Pagamentos pagamento) {
+        return pagamentosService.savePagamento(pagamento);
+    }
+}
