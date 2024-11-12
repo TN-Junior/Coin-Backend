077cf4a0e92a5d0a8ecbb3e672c27ac6fcd386a7
tn-junior <juju2004.tary@gmail.com>
2024-11-12 12:31:12 -0300

    adicionando url deploy railway

--- src/main/java/com/example/coin/config/WebConfig.java
+++ src/main/java/com/example/coin/config/WebConfig.java
@@ -16,6 +16,7 @@ public class WebConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("https://coin-pa.vercel.app/");
+        config.addAllowedOrigin("https://web-coin-railway-git-main-tarynascimentos-projects.vercel.app/");
         config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
