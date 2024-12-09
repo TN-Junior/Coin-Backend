e18286d8bbfb3b739dd50aad63c55c1a6c4d2bb4
tn-junior <juju2004.tary@gmail.com>
2024-12-09 15:34:56 -0300

    fix: ajustes

--- src/main/java/com/example/coin/config/WebConfig.java
+++ src/main/java/com/example/coin/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig {
         config.addAllowedOrigin("https://web-coin-railway-git-main-tarynascimentos-projects.vercel.app/");
         config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("https://coin-frontend2-0.vercel.app");
-        config.addAllowedOrigin("https://coinfrontend.vercel.app/dashboard");
+        config.addAllowedOrigin("https://coinfrontend.vercel.app");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
